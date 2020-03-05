6605c24179002dbdda24eb2e15fe52aa40921d68
kms0311 <kms0311@midasit.com>
2020-03-05 19:03:29 +0900

    나와잘맞는친구 만드는중

--- src/pages/main/mainInfo/mainInfo.jsx
+++ src/pages/main/mainInfo/mainInfo.jsx
@@ -11,7 +11,7 @@ class mainInfo extends Component {
                     <div className={style.stepItem}>
                         <div><span className={style.stepNumber}>1</span></div>
                         <div className={style.stepImg}><img src="/img/mainStepIcon1.png" title="이용설명 step1" alt="이용설명 step1"></img></div>
-                        <div className={style.stepTitle}></div>
+                        <div className={style.stepTitle}>성향 설문</div>
                         <div className={style.stepContent}>딱 10분 ! 간단하게 체크하고<br/>나와 잘 맞는 친구 만들기</div>
                     </div>
                 </div>
