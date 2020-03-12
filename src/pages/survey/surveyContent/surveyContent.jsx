62415f94ee9a4d1688c8d58262e4828dd38a7751
kms0311 <kms0311@midasit.com>
2020-03-12 15:36:42 +0900

    프로그레스 대충 만듬

--- src/pages/survey/surveyContent/surveyContent.jsx
+++ src/pages/survey/surveyContent/surveyContent.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import style from './surveyContent.module.scss';
 import ContentBox from '../../../commonComponent/contentBox/contentBox';
 import MainTitleH1 from '../../../commonComponent/mainTitle/mainTitleH1';
+import './radialProgress.scss';
 
 class surveyContent extends Component {
     render() {
@@ -10,12 +11,8 @@ class surveyContent extends Component {
                 <ContentBox>
                     <MainTitleH1 fontSize="36px">친구 찾기</MainTitleH1>
                     <div className={style.surveyHead}>
-                        <div className={style.faceGraph}>
-                            <div className={style.faceGraphInner}>
-                                <div className={style.progress}>
-
-                                </div>
-                            </div>
+                        <div className={[style.faceGraph, 'radialProgress-60'].join(' ')}>
+                            <div className={style.faceGraphInner}></div>
                         </div>
                     </div>
                 </ContentBox>
