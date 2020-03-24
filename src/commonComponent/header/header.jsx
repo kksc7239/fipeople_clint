16b77d07bb34dc9e3bc60ae8f7c1885b394f68e4
kms0311 <kms0311@midasit.com>
2020-03-24 18:42:03 +0900

    axios , mobx, mobx-react 추가, 카카오로그인 링크 붙여둠

--- src/commonComponent/header/header.jsx
+++ src/commonComponent/header/header.jsx
@@ -26,7 +26,7 @@ class header extends Component {
                                 <li><Link to='#'>로그인</Link></li>
                             </ul> */}
                             {/* <Link to='#'><img src="/img/kakao_login_btn_small.png" alt="카카오톡 로그인하기" title="카카오톡 로그인하기"></img></Link> */}
-                            <Link to='#'><img src="/img/kakao_login_btn_small_ko.png" alt="카카오톡 로그인하기" title="카카오톡 로그인하기"></img></Link>
+                            <a href='http://ec2-13-209-174-48.ap-northeast-2.compute.amazonaws.com/api/auth/oauth-kakao-init' target="_blank"><img src="/img/kakao_login_btn_small_ko.png" alt="카카오톡 로그인하기" title="카카오톡 로그인하기"></img></a>
                         </nav>
                     </div>
                 </div>
