4b6c6438e537976dc64988ec9dc49e58f1a52fde
myeongseok <kksc7239@gmail.com>
2020-04-19 20:09:38 +0900

    토큰 재발급

--- src/common/httpRequest.js
+++ src/common/httpRequest.js
@@ -8,18 +8,38 @@ const httpRequest = axios.create({
 function get(url, body, option) {
     let requestOption = {}
     if (option.token) {
-        requestOption.Authorization = 'Bearer ' + localStorage.getItem('accessToken') //토큰넣어야댐
+        requestOption.Authorization = 'Bearer ' + localStorage.getItem('accessToken')
     }
-
-    return httpRequest.get(url, {params : body, headers: requestOption})
+    return new Promise((resolve, reject) => {
+        httpRequest.get(url, {params : body, headers: requestOption}).then((data) => {
+            resolve(data);
+        }).catch((error) => {
+            if(error.response.data.message === 'ACCESS_TOKEN_EXPIRED') {
+                httpRequest.post('/api/auth/refresh-token', {refreshToken : localStorage.getItem('refreshToken')}, {headers: requestOption}).then((res) => {
+                    localStorage.setItem("accessToken", res.data.accessToken);
+                    resolve(get(url, body, option));
+                })
+            }
+        })
+    })
 }
 function post(url, body, option) {
     let requestOption = {}
     if (option.token) {
-        requestOption.Authorization = 'Bearer ' + localStorage.getItem('accessToken') //토큰넣어야댐
+        requestOption.Authorization = 'Bearer ' + localStorage.getItem('accessToken')
     }
-
-    return httpRequest.post(url, body, {headers: requestOption})
+    return new Promise((resolve, reject) => {
+        httpRequest.post(url, body, {headers: requestOption}).then((data) => {
+            resolve(data);
+        }).catch((error) => {
+            if(error.response.data.message === 'ACCESS_TOKEN_EXPIRED') {
+                httpRequest.post('/api/auth/refresh-token', {refreshToken : localStorage.getItem('refreshToken')}, {headers: requestOption}).then((res) => {
+                    localStorage.setItem("accessToken", res.data.accessToken);
+                    resolve(post(url, body, option));
+                })
+            }
+        })
+    })
 }
 
 export { get, post };
